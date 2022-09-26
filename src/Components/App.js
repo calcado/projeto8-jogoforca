c3439abcd800e57892ff812d376b49fc68d187f2
calcado <pecalcado@gmail.com>
2022-09-26 17:29:32 -0300

    versao com funcoes implementadas

--- src/Components/App.js
+++ src/Components/App.js
@@ -9,75 +9,140 @@ import erro4 from '../assets/forca4.png'
 import erro5 from '../assets/forca5.png'
 import erro6 from '../assets/forca6.png'
 
+function Letra(props){
+    const [habilitado, setDesabilitar] = useState('habilitado')
+    console.log(habilitado)
+    console.log(props.jogoIniciado)
+    return(
+        <li>
+                <button className={`cor letrinha ${habilitado}`}
+                    disabled={props.jogoIniciado && habilitado ==='habilitado' ? false :true }  onClick={()=>props.SelecionarLetra(props.letra,setDesabilitar)}>{props.letra.toUpperCase()}
+                </button>
+        </li>)
 
+    }
+    
+    function Teclado(props){
+        
+        const [input, setInput] = useState('')
+        function handleChange(evento){
+            setInput(evento.target.value)
+        }
+        function Chute(){
+            console.log(props.palavraSorteada)
+            if(input === props.palavraSorteada){
+                props.setUnderline(input)
+                props.setCor("palavraEstilo verde")  
+                
+            }else{
+                props.setUnderline(props.palavraSorteada)
+                props.setCor("palavraEstilo vermelho")
+            }
+        }
+
+        return(
+
+            <div className='teclado'>
+                <p>Ja sei a palavra</p>
+                <input className="input" type="text" name="description" onChange={handleChange} value={input}/>
+                <button className='tentativa' onClick={()=> Chute()}> Chutar </button>
+            </div>
+
+        )
+    }
 export default function App(){
     const alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-    let erro = 0
+    
     const imagens =[forca0,erro1,erro2,erro3,erro4,erro5,erro6]
     
     const [palavraAleatoria, setpalavraAleatoria] = useState([]) 
     const [underline, setUnderline] = useState([palavraAleatoria])
-    
-    const [imagem, setImagem] = useState(forca0)
+    const [cor, setCor] = useState('palavraEstilo')
     const [clicada, setClicada] =useState([]) 
     const [erros, setContador] = useState(0)
     const [input, setInput] = useState('')
+    const [acerto, setAcerto] = useState(0)
+    const [palavraSorteada, setSorteada] = useState('')
+    const [jogoIniciado, setJogo] = useState(false)
+    const [arrayClicados, setArrayClicados] = useState([])
     
-   function Errou(letra){
     
-    setContador(erros+1)
-    
-    setImagem(imagens[{erros}])
-   }
-
-   function Venceu(){
-    alert("alou")
-    
-   }
-   function Perdeu(){
-    alert("perdeu")
-   }
-   
-    function Chute(){
-        const arrInput = input.split('')
-        if(arrInput === palavraAleatoria){
-            alert('oi')
-        }else{
-            alert('nonom')
-        }
-
-        console.log(arrInput)
-        console.log(palavraAleatoria)
-    }
-   
-    function SelecionarLetra(letra,setDesabilitar){
-        setDesabilitar('')
-        alert(letra);
-        const arrayNova =[...underline]
+    // function Chute(){
                 
+    //     if(input === palavraSorteada){
+    //         setUnderline(palavraSorteada)
+    //         setCor("palavraEstilo verde")  
+            
+    //     }else{
+    //         setUnderline(palavraSorteada)
+    //         setCor("palavraEstilo vermelho")
+    //     }
+    // }
+    function Resetar(){
+        setpalavraAleatoria([])
+        setUnderline(palavraAleatoria)
+        setCor('palavraEstilo')
+        setClicada([])
+        setInput('')
+        setContador(0)
+        setAcerto(0)
+        setJogo(false)
+        setArrayClicados([])
+
+    }
+
+    console.log(arrayClicados)
+
+    function SelecionarLetra(letra,setDesabilitar){
+        setArrayClicados([...arrayClicados,letra])
+        setDesabilitar('')
+        
+        const arrayNova =[...underline]
+        let acertos = 0
+        let erro = 0        
         if(palavraAleatoria.includes(letra) ){
             for(let i=0; i < palavraAleatoria.length; i++){
                 if(palavraAleatoria[i]===letra){
                     arrayNova[i] = letra;
                     setUnderline(arrayNova)
+                    acertos++
+                    
                 }     
-             }
+            }
+            acertos=acertos+acerto
+            setAcerto(acertos)
+            
+
         }
         else{
-            setContador(erros+1)
-            alert('não') 
+            
+            erro = erros +1
+            setContador(erro)
+             
         }     
-        
-        if(!underline.includes("_") ){
-            Venceu()
-        }else if(erros>= 5){
-            alert("perdeu")
-        }    
-    }   
+        if(acertos === palavraSorteada.length){
+            setUnderline(palavraSorteada)
+            setCor("palavraEstilo verde") 
+        }
+        if(erro>= 6){
+            alert()
+            setCor("palavraEstilo vermelho")
+            setUnderline(palavraSorteada)
+        } 
+         
+    }
+    console.log("acerto:"+acerto)
+    console.log("erros:"+erros)
+    console.log(palavraSorteada.length)   
         
     function EscolherPalavra(){
+        
+        Resetar()
+        setJogo(true)
+        
         let randomIndice = Math.floor(Math.random()*palavras.length)
         const palavra = palavras[randomIndice]
+        setSorteada(palavra)
         const palavraNormal = palavra.normalize('NFD').replace(/[\u0300-\u036f]/g, "")
         const palavraArray = palavraNormal.split("")
         setpalavraAleatoria(palavraArray);
@@ -85,18 +150,35 @@ export default function App(){
         setUnderline([...arrUnderline])
         console.log(palavra)
     }
-    function Letra(props){
-        const [habilitado, setDesabilitar] = useState('habilitado')
-        console.log(habilitado)
-        return(
-            <li>
-                    <button className={`letrinha ${habilitado}`}
-                      disabled={habilitado != "habilitado" ? true : false }  onClick={()=>SelecionarLetra(props.letra,setDesabilitar)}>{props.letra.toUpperCase()}
-                    </button>
-            </li>)
+    // function Letra(props){
+    //     const [habilitado, setDesabilitar] = useState('habilitado')
+    //     console.log(habilitado)
+    //     return(
+    //         <li>
+    //                 <button className={`cor letrinha ${habilitado}`}
+    //                   disabled={habilitado != "habilitado" ? true : false }  onClick={()=>SelecionarLetra(props.letra,setDesabilitar)}>{props.letra.toUpperCase()}
+    //                 </button>
+    //         </li>)
     
-        }
+    //     }
+   function handleChange(evento){
+    setInput(evento.target.value)
+
+   }
+    // function Teclado(props){
         
+    //     return(
+
+    //         <div className='teclado'>
+    //             <p>Ja sei a palavra</p>
+    //             <input className="input" type="text" name="description" onChange={handleForm} value={form.description}/>
+    //             {/* <input className="input"  type ='text' value={input} placeholder='' onChange={handleChange}/> */}
+    //             <button className='tentativa' onClick={()=> Chute()}> Chutar </button>
+    //         </div>
+
+    //     )
+    // }
+      
     return (
         <>
         
@@ -106,20 +188,21 @@ export default function App(){
             <div className='direita'>
                 <button className='escolherPalavra' 
                 onClick={EscolherPalavra}>Escolher Palavra</button>
-                <p className='palavraAleatoria'>{underline}</p>
+                <p className={cor }>{underline}</p>
             </div>
         </div>
 
         <div className='baixo'>
-            <ul className='letras'>{alfabeto.map((letra, indice)=><Letra letra={letra}  key={indice}/>)}              
+            <ul className='letras'>{alfabeto.map((letra, indice)=><Letra SelecionarLetra={SelecionarLetra} jogoIniciado={jogoIniciado} letra={letra}  key={indice}/>)}              
             </ul>
+            <Teclado setUnderline={setUnderline} 
+            setCor={setCor} 
+            setpalavraAleatoria={setpalavraAleatoria} 
+            palavraSorteada = {palavraSorteada} 
             
+            />
+          
             
-            <div className='teclado'>
-                <p>Ja sei a palavra</p>
-                <input type ='text' placeholder='' onChange={e => setInput(e.target.value)}/>
-                <button className='Chute' onClick={Chute}> Chutar </button>
-            </div>
         </div>
         
         </>
